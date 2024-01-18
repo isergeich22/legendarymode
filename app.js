04d57b69d3c6b916496997bf9665707848b06a12
ilya <skiing1r@gmail.com>
2024-01-18 21:43:10 +0300

    update commit

--- app.js
+++ app.js
@@ -20,38 +20,38 @@ const urlencodedParser = express.urlencoded({extended: false})
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', async function(req, res){
+// app.get('/', async function(req, res){
 
-    let startPageContent = `<!DOCTYPE html>
-                            <html lang="en">
-                            <head>
-                                <meta charset="UTF-8">
-                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                <title>Турнир - Легендарная сложность</title>
-                                <link rel="stylesheet" href="/style.css">
-                                <link rel="stylesheet" href="/components/header/header.css">
-                                <link rel="stylesheet" href="/components/footer/footer.css">
-                                <link rel="stylesheet" href="/components/auth/auth.css"
-                                <link rel="stylesheet" href="/components/news/news.css">
-                                <link rel="stylesheet" href="/components/nav/nav.css">
-                                <link rel="stylesheet" href="/components/about/about.css">
-                                <link rel="stylesheet" href="/components/participant/participant.css">
-                                <link rel="stylesheet" href="/components/seasons/seasons.css">
-                                <link rel="stylesheet" href="/components/eventBlock/event.css">
-                                <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-                                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-                            </head>
-                            <body>`
+//     let startPageContent = `<!DOCTYPE html>
+//                             <html lang="en">
+//                             <head>
+//                                 <meta charset="UTF-8">
+//                                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//                                 <title>Турнир - Легендарная сложность</title>
+//                                 <link rel="stylesheet" href="/style.css">
+//                                 <link rel="stylesheet" href="/components/header/header.css">
+//                                 <link rel="stylesheet" href="/components/footer/footer.css">
+//                                 <link rel="stylesheet" href="/components/auth/auth.css"
+//                                 <link rel="stylesheet" href="/components/news/news.css">
+//                                 <link rel="stylesheet" href="/components/nav/nav.css">
+//                                 <link rel="stylesheet" href="/components/about/about.css">
+//                                 <link rel="stylesheet" href="/components/participant/participant.css">
+//                                 <link rel="stylesheet" href="/components/seasons/seasons.css">
+//                                 <link rel="stylesheet" href="/components/eventBlock/event.css">
+//                                 <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+//                                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+//                             </head>
+//                             <body>`
 
-    let endPageContent = `<script src="/script.js"></script>
-                        </body>
-                    </html>`
+//     let endPageContent = `<script src="/script.js"></script>
+//                         </body>
+//                     </html>`
 
-    res.send(startPageContent + auth.auth + endPageContent)
+//     res.send(startPageContent + auth.auth + endPageContent)
 
-})
+// })
 
-app.get('/home', async function(req, res){
+app.get(['/', '/home'], async function(req, res){
 
     let seasonNav = ``
 
