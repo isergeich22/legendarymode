adab23ced54c300acbc2e777a75c7fa1d05d46ae
ilya <skiing1r@gmail.com>
2024-01-20 22:55:49 +0300

    add adaptive for mobile

--- public/components/news/news.js
+++ public/components/news/news.js
@@ -1,6 +1,8 @@
 exports.news = news = (content) =>`
     <section id="news" class="news">
-        <h1 class="news-header">Новости турнира</h1>
+        <div class="news-header">
+            <h1 class="news-header__text">Новости турнира</h1>
+        </div>
         ${content}
     </section>
 `
