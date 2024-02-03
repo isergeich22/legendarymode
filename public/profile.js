ada1e784a6fbc724490f642fb8887b4cc6890b79
ilya <skiing1r@gmail.com>
2024-02-04 00:36:58 +0300

    update commit

--- public/profile.js
+++ public/profile.js
@@ -20,37 +20,73 @@ events.forEach(el => {
 
                 if(el.parentNode.children[1].classList.contains('empty') === true) {
 
-                    el.parentNode.children[1].innerHTML = `
-                        <div id="video" class="profile-info-events-list__item-detailed__video">
-                            <iframe width="500" height="281" src="${event.eventVideo[event.eventVideo.findIndex(evt => evt.videoAuthor === event.eventHost)].videoLink}" title="YouTube video player" 
-                                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-                                gyroscope; picture-in-picture; web-share" allowfullscreen>
-                            </iframe>
-                        </div>
-                        <div class="profile-info-events-list__item-detailed__desc">
-                            <div class="profile-info-events-list__item-detailed__desc-item">
-                                <p>Название ивента:</p>
-                                <p>${event.eventTitle}</p>
-                            </div>
-                            <div class="profile-info-events-list__item-detailed__desc-item">
-                                <p>Дата ивента:</p>
-                                <p>${event.eventDate}</p>
-                            </div>
-                            <div class="profile-info-events-list__item-detailed__desc-item">
-                                <p>Игра ивента:</p>
-                                <p>${event.eventGame}</p>
-                            </div>
-                            <div class="profile-info-events-list__item-detailed__desc-item">
-                                <p>Краткие правила:</p>
-                                <p>${event.eventDescription}</p>
-                            </div>
-                            <div class="profile-info-events-list__item-detailed__desc-hide">
-                                <button id="hide">
-                                    Скрыть подробности
-                                </button>
-                            </div>
-                        </div>                        
-                        `                        
+                    if(event.eventVideo.findIndex(evt => evt.videoAuthor === event.eventHost) >= 0) {
+
+                        el.parentNode.children[1].innerHTML += `
+                            <div id="video" class="profile-info-events-list__item-detailed__video">
+                                <iframe width="500" height="281" src="${event.eventVideo[event.eventVideo.findIndex(evt => evt.videoAuthor === event.eventHost)].videoLink}" title="YouTube video player" 
+                                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+                                    gyroscope; picture-in-picture; web-share" allowfullscreen>
+                                </iframe>
+                            </div>`
+
+                        el.parentNode.children[1].innerHTML += `
+                            <div class="profile-info-events-list__item-detailed__desc">
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Название ивента:</p>
+                                    <p>${event.eventTitle}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Дата ивента:</p>
+                                    <p>${event.eventDate}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Игра ивента:</p>
+                                    <p>${event.eventGame}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Краткие правила:</p>
+                                    <p>${event.eventDescription}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-hide">
+                                    <button id="hide">
+                                        Скрыть подробности
+                                    </button>
+                                </div>
+                            </div>                        
+                            `
+
+                        }
+
+                        if(event.eventVideo.findIndex(evt => evt.videoAuthor === event.eventHost) < 0) {
+                            
+                            el.parentNode.children[1].innerHTML += `
+                            <div class="profile-info-events-list__item-detailed__desc">
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Название ивента:</p>
+                                    <p>${event.eventTitle}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Дата ивента:</p>
+                                    <p>${event.eventDate}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Игра ивента:</p>
+                                    <p>${event.eventGame}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-item">
+                                    <p>Краткие правила:</p>
+                                    <p>${event.eventDescription}</p>
+                                </div>
+                                <div class="profile-info-events-list__item-detailed__desc-hide">
+                                    <button id="hide">
+                                        Скрыть подробности
+                                    </button>
+                                </div>
+                            </div>                        
+                            `
+
+                        }
 
                         const hide = document.querySelector('#hide')
 
